8a02b4e8828e9d59f1ef516ec2f09fc9faa9f799
Ed Stokes <edstokes96@gmail.com>
2023-03-06 13:02:26 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const name = ["Guadalupe", "Ollie", "Aki"]
+const gift = "surprise"
+
+function writeCards(name, gift) {
+    let thankYou = []
+    for (let i = 0; i < name.length; i++) {
+        thankYou.push(`Thank you, ${name[i]}, for the wonderful ${gift} gift!`)
+    }
+    return thankYou
+}
+
+function countDown() {
+    let number = 11
+    while (number--) {
+        console.log(number)
+    }
+    
+
+}
